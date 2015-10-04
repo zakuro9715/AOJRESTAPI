99dc3571e582b8a016807ea27100c86104c70524
YuZakuro <zakuro@yuzakuro.me>
2015-10-04 21:54:48 +0900

    Add Server.HandlerFunc()

--- web/router/router.go
+++ web/router/router.go
@@ -1,4 +1,12 @@
 package router
 
+import (
+	"net/http"
+)
+
 type Router struct {
+}
+
+func (r *Router) HandlerFunc() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {}
 }
